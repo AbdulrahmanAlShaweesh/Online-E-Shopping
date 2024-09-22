52944a2f6a28f0cb7c99cdf1dafc0ac1385391b3
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-09-22 10:22:06 +0300

    added contact controller

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using onlineEShopping.Data;
 
@@ -12,12 +13,18 @@ namespace onlineEShopping.Controllers
     
     public class ContactController : Controller
     {
-         private readonly OnlineEShoppingDbContext _dbContext;
+        private readonly OnlineEShoppingDbContext _dbContext;
 
-         public ContactController(OnlineEShoppingDbContext dbContext)
-         {  
-            _dbContext = dbContext;
-         }
+        public ContactController(OnlineEShoppingDbContext dbContext)
+        {  
+        _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index() {
+            var response = await _dbContext.Contacts.ToListAsync(); 
+            
+            return View(response);
+        }
 
     }
 }
